34e23dfbeaea600cd385ca5e1e4c2386a66bdad9
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-02-27 22:29:51 -0500

    Added CSS for background Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import { useState } from 'react'
-import './App.css'
-
-
-
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  return (
-    <div className='card-component'>
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'}
-      </button>
-      
-
-    </div>
-  )
-}
-
 
 
 const App = () => {
   return (
-    <div className='card-container'>
-      <Card title = "The Batman" />
-      <Card title= "Top Gun"/>
-      <Card title = "Mission Impossible"/>
-
+    <div>
+    <h1 className="text-3xl font-bold underline text-red-500">
+    Hello world!
+    </h1>
     </div>
   )
 }
 
 export default App
-
-
-
-
-
